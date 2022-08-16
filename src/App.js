9cc08b38dca6e697c538662bd778745e56f41be0
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-08-16 22:11:43 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,176 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import myEpicNft from './utils/MyEpicNFT.json';
+
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'catosheeth';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x7d6666C491aE0F81C332cAd97E46e31C59780427";
+
 
 const App = () => {
-  // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+
+
+  /*
+  * Gotta make sure this is async.
+  */
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+  
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4"; 
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+    
+    if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+    } else {
+        console.log("We have the ethereum object", ethereum);
+    }
+
+    /*
+    * Check if we're authorized to access the user's wallet
+    */
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /*
+    * User can have multiple authorized accounts, we grab the first one if its there!
+    */
+    if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        // Setup listener! This is for the case where a user comes to our site
+        // and ALREADY had their wallet connected + authorized.
+         setupEventListener()
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+
+
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+    // Setup our listener.
+    const setupEventListener = async () => {
+      // Most of this looks the same as our function askContractToMintNft
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          // Same stuff again
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+          // THIS IS THE MAGIC SAUCE.
+          // This will essentially "capture" our event when our contract throws it.
+          // If you're familiar with webhooks, it's very similar to that!
+          connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+            console.log(from, tokenId.toNumber())
+            alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          });
+  
+          console.log("Setup event listener!")
+  
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+  const askContractToMintNft = async () => {
+  
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+  
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+  
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+    // Render Methods
+    const renderNotConnectedContainer = () => (
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect to Wallet
+      </button>
+    );
+
+    const renderMintUI = () => (
+      <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+    )
+
+  /*
+  * This runs our function when the page loads.
+  */
+    useEffect(() => {
+      checkIfWalletIsConnected();
+    }, [])
+  
 
   return (
     <div className="App">
@@ -24,7 +180,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
